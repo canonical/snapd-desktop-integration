305b0920f0352c5d6a8c4b4bd3e9d41edc70a1a4
Robert Ancell <robert.ancell@canonical.com>
2023-05-16 14:26:15 +1200

    Use snake case, not camel case for variable names

--- src/refresh_status.h
+++ src/refresh_status.h
@@ -24,32 +24,32 @@
 
 typedef struct {
   DsState *dsstate;
-  GString *appName;
+  GString *app_name;
   GtkApplicationWindow *window;
-  GtkWidget *progressBar;
+  GtkWidget *progress_bar;
   GtkLabel *message;
   GtkWidget *icon;
-  gchar *lockFile;
-  guint timeoutId;
-  guint closeId;
+  gchar *lock_file;
+  guint timeout_id;
+  guint close_id;
   gboolean pulsed;
   gboolean wait_change_in_lock_file;
   gint width;
   gint height;
 } RefreshState;
 
-void handle_application_is_being_refreshed(const gchar *appName,
-                                           const gchar *lockFilePath,
-                                           GVariant *extraParams,
+void handle_application_is_being_refreshed(const gchar *app_name,
+                                           const gchar *lock_file_path,
+                                           GVariant *extra_params,
                                            DsState *ds_state);
-void handle_close_application_window(const gchar *appName,
-                                     GVariant *extraParams, DsState *ds_state);
-void handle_set_pulsed_progress(const gchar *appName, const gchar *barText,
-                                GVariant *extraParams, DsState *ds_state);
-void handle_set_percentage_progress(const gchar *appName, const gchar *barText,
-                                    gdouble percentage, GVariant *extraParams,
-                                    DsState *ds_state);
-RefreshState *refresh_state_new(DsState *state, const gchar *appName);
+void handle_close_application_window(const gchar *app_name,
+                                     GVariant *extra_params, DsState *ds_state);
+void handle_set_pulsed_progress(const gchar *app_name, const gchar *bar_text,
+                                GVariant *extra_params, DsState *ds_state);
+void handle_set_percentage_progress(const gchar *app_name,
+                                    const gchar *bar_text, gdouble percentage,
+                                    GVariant *extra_params, DsState *ds_state);
+RefreshState *refresh_state_new(DsState *state, const gchar *app_name);
 
 void refresh_state_free(RefreshState *state);
 
