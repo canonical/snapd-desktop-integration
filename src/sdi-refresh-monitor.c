994958984ccc7b859f3b58b020eb9aced051fd44
Robert Ancell <robert.ancell@canonical.com>
2023-06-09 17:02:48 +1200

    Refactor SdiRefreshDialog so it is decoupled from SdiRefreshMonitor

--- src/sdi-refresh-monitor.c
+++ src/sdi-refresh-monitor.c
@@ -27,49 +27,126 @@ struct _SdiRefreshMonitor {
 
   SnapDesktopIntegration *skeleton;
 
-  /* The list of current refresh popups */
-  GList *refreshing_list;
+  GList *dialogs;
 };
 
 G_DEFINE_TYPE(SdiRefreshMonitor, sdi_refresh_monitor, G_TYPE_OBJECT)
 
-static gboolean dbus_handle_application_is_being_refreshed(
-    SnapDesktopIntegration *skeleton, GDBusMethodInvocation *invocation,
-    gchar *snap_name, gchar *lockFilePath, GVariant *extra_params,
-    gpointer data) {
-  handle_application_is_being_refreshed(snap_name, lockFilePath, extra_params,
-                                        data);
-  snap_desktop_integration_complete_application_is_being_refreshed(skeleton,
-                                                                   invocation);
+static SdiRefreshDialog *lookup_dialog(SdiRefreshMonitor *self,
+                                       const char *app_name) {
+  for (GList *link = self->dialogs; link != NULL; link = link->next) {
+    SdiRefreshDialog *dialog = (SdiRefreshDialog *)link->data;
+    if (0 == g_strcmp0(sdi_refresh_dialog_get_app_name(dialog), app_name)) {
+      return dialog;
+    }
+  }
+  return NULL;
+}
+
+static void handle_extra_params(SdiRefreshMonitor *self,
+                                SdiRefreshDialog *dialog,
+                                GVariant *extra_params) {
+  GVariantIter iter;
+  GVariant *value;
+  const gchar *key;
+
+  // Do a copy to allow manage the iter in other places if needed
+  g_variant_iter_init(&iter, extra_params);
+  while (g_variant_iter_next(&iter, "{&sv}", &key, &value)) {
+    if (!g_strcmp0(key, "message")) {
+      sdi_refresh_dialog_set_message(dialog, g_variant_get_string(value, NULL));
+    } else if (!g_strcmp0(key, "title")) {
+      sdi_refresh_dialog_set_title(dialog, g_variant_get_string(value, NULL));
+    } else if (!g_strcmp0(key, "icon")) {
+      sdi_refresh_dialog_set_icon(dialog, g_variant_get_string(value, NULL));
+    } else if (!g_strcmp0(key, "icon_image")) {
+      sdi_refresh_dialog_set_icon_image(dialog,
+                                        g_variant_get_string(value, NULL));
+    } else if (!g_strcmp0(key, "wait_change_in_lock_file")) {
+      sdi_refresh_dialog_set_wait_change_in_lock_file(dialog);
+    } else if (!g_strcmp0(key, "desktop_file")) {
+      sdi_refresh_dialog_set_desktop_file(dialog,
+                                          g_variant_get_string(value, NULL));
+    }
+    g_variant_unref(value);
+  }
+}
+
+static gboolean dialog_destroy_cb(SdiRefreshMonitor *self,
+                                  SdiRefreshDialog *dialog) {
+  self->dialogs = g_list_remove(self->dialogs, dialog);
   return TRUE;
 }
 
-static gboolean dbus_handle_application_refresh_completed(
-    SnapDesktopIntegration *skeleton, GDBusMethodInvocation *invocation,
-    gchar *snap_name, GVariant *extra_params, gpointer data) {
-  handle_application_refresh_completed(snap_name, extra_params, data);
-  snap_desktop_integration_complete_application_refresh_completed(skeleton,
-                                                                  invocation);
+static gboolean handle_application_is_being_refreshed_cb(
+    SdiRefreshMonitor *self, GDBusMethodInvocation *invocation,
+    const gchar *snap_name, const gchar *lock_file_path,
+    GVariant *extra_params) {
+  SdiRefreshDialog *dialog = lookup_dialog(self, snap_name);
+
+  if (dialog != NULL) {
+    gtk_window_present(GTK_WINDOW(dialog));
+    handle_extra_params(self, dialog, extra_params);
+  } else {
+    dialog = sdi_refresh_dialog_new(snap_name, lock_file_path);
+    self->dialogs = g_list_append(self->dialogs, dialog);
+    g_signal_connect_swapped(G_OBJECT(dialog), "destroy",
+                             G_CALLBACK(dialog_destroy_cb), self);
+    gtk_window_present(GTK_WINDOW(dialog));
+    handle_extra_params(self, dialog, extra_params);
+  }
+
+  snap_desktop_integration_complete_application_is_being_refreshed(
+      self->skeleton, invocation);
+
   return TRUE;
 }
 
-static gboolean dbus_handle_set_pulsed_progress(
-    SnapDesktopIntegration *skeleton, GDBusMethodInvocation *invocation,
-    gchar *snap_name, gchar *bar_text, GVariant *extra_params, gpointer data) {
-  handle_set_pulsed_progress(snap_name, bar_text, extra_params, data);
-  snap_desktop_integration_complete_application_refresh_pulsed(skeleton,
+static gboolean handle_application_refresh_completed_cb(
+    SdiRefreshMonitor *self, GDBusMethodInvocation *invocation,
+    const gchar *snap_name, GVariant *extra_params) {
+  SdiRefreshDialog *dialog = lookup_dialog(self, snap_name);
+  if (dialog != NULL) {
+    gtk_window_destroy(GTK_WINDOW(dialog));
+  }
+
+  snap_desktop_integration_complete_application_refresh_completed(
+      self->skeleton, invocation);
+
+  return TRUE;
+}
+
+static gboolean handle_set_pulsed_progress_cb(SdiRefreshMonitor *self,
+                                              GDBusMethodInvocation *invocation,
+                                              const gchar *snap_name,
+                                              const gchar *bar_text,
+                                              GVariant *extra_params) {
+  SdiRefreshDialog *dialog = lookup_dialog(self, snap_name);
+  if (dialog != NULL) {
+    sdi_refresh_dialog_set_pulsed_progress(dialog, bar_text);
+    handle_extra_params(self, dialog, extra_params);
+  }
+
+  snap_desktop_integration_complete_application_refresh_pulsed(self->skeleton,
                                                                invocation);
+
   return TRUE;
 }
 
-static gboolean dbus_handle_set_percentage_progress(
-    SnapDesktopIntegration *skeleton, GDBusMethodInvocation *invocation,
-    gchar *snap_name, gchar *bar_text, gdouble percentage,
-    GVariant *extra_params, gpointer data) {
-  handle_set_percentage_progress(snap_name, bar_text, percentage, extra_params,
-                                 data);
-  snap_desktop_integration_complete_application_refresh_percentage(skeleton,
-                                                                   invocation);
+static gboolean
+handle_set_percentage_progress_cb(SdiRefreshMonitor *self,
+                                  GDBusMethodInvocation *invocation,
+                                  const gchar *snap_name, const gchar *bar_text,
+                                  gdouble percentage, GVariant *extra_params) {
+  SdiRefreshDialog *dialog = lookup_dialog(self, snap_name);
+  if (dialog != NULL) {
+    sdi_refresh_dialog_set_percentage_progress(dialog, bar_text, percentage);
+    handle_extra_params(self, dialog, extra_params);
+  }
+
+  snap_desktop_integration_complete_application_refresh_percentage(
+      self->skeleton, invocation);
+
   return TRUE;
 }
 
@@ -77,11 +154,11 @@ static void sdi_refresh_monitor_dispose(GObject *object) {
   SdiRefreshMonitor *self = SDI_REFRESH_MONITOR(object);
 
   g_clear_object(&self->skeleton);
-  for (GList *link = self->refreshing_list; link != NULL; link = link->next) {
+  for (GList *link = self->dialogs; link != NULL; link = link->next) {
     SdiRefreshDialog *dialog = (SdiRefreshDialog *)link->data;
     gtk_window_destroy(GTK_WINDOW(dialog));
   }
-  g_list_free(self->refreshing_list);
+  g_list_free(self->dialogs);
 
   G_OBJECT_CLASS(sdi_refresh_monitor_parent_class)->dispose(object);
 }
@@ -102,38 +179,18 @@ SdiRefreshMonitor *sdi_refresh_monitor_new() {
 gboolean sdi_refresh_monitor_start(SdiRefreshMonitor *self,
                                    GDBusConnection *connection,
                                    GError **error) {
-  g_signal_connect(self->skeleton, "handle_application_is_being_refreshed",
-                   G_CALLBACK(dbus_handle_application_is_being_refreshed),
-                   self);
-  g_signal_connect(self->skeleton, "handle_application_refresh_completed",
-                   G_CALLBACK(dbus_handle_application_refresh_completed), self);
-  g_signal_connect(self->skeleton, "handle_application_refresh_pulsed",
-                   G_CALLBACK(dbus_handle_set_pulsed_progress), self);
-  g_signal_connect(self->skeleton, "handle_application_refresh_percentage",
-                   G_CALLBACK(dbus_handle_set_percentage_progress), self);
+  g_signal_connect_swapped(
+      self->skeleton, "handle_application_is_being_refreshed",
+      G_CALLBACK(handle_application_is_being_refreshed_cb), self);
+  g_signal_connect_swapped(
+      self->skeleton, "handle_application_refresh_completed",
+      G_CALLBACK(handle_application_refresh_completed_cb), self);
+  g_signal_connect_swapped(self->skeleton, "handle_application_refresh_pulsed",
+                           G_CALLBACK(handle_set_pulsed_progress_cb), self);
+  g_signal_connect_swapped(self->skeleton,
+                           "handle_application_refresh_percentage",
+                           G_CALLBACK(handle_set_percentage_progress_cb), self);
   return g_dbus_interface_skeleton_export(
       G_DBUS_INTERFACE_SKELETON(self->skeleton), connection,
       "/io/snapcraft/SnapDesktopIntegration", error);
-}
-
-SdiRefreshDialog *
-sdi_refresh_monitor_lookup_application(SdiRefreshMonitor *self,
-                                       const char *app_name) {
-  for (GList *link = self->refreshing_list; link != NULL; link = link->next) {
-    SdiRefreshDialog *dialog = (SdiRefreshDialog *)link->data;
-    if (0 == g_strcmp0(sdi_refresh_dialog_get_app_name(dialog), app_name)) {
-      return dialog;
-    }
-  }
-  return NULL;
-}
-
-void sdi_refresh_monitor_add_application(SdiRefreshMonitor *self,
-                                         SdiRefreshDialog *dialog) {
-  self->refreshing_list = g_list_append(self->refreshing_list, dialog);
-}
-
-void sdi_refresh_monitor_remove_application(SdiRefreshMonitor *self,
-                                            SdiRefreshDialog *dialog) {
-  self->refreshing_list = g_list_remove(self->refreshing_list, dialog);
 }
