90dc0ebcb43046d47e739c8f8bdd5b8febd47764
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2024-06-18 14:17:49 +0200

    Use DBus directly for Unity protocol
    The libunity library detects if the program is inside a snap,
    in which case it modifies the passed .desktop file to match the
    expected one in a snapped program. Unfortunately, this prevents
    us to pose as other programs to put a progress bar, so we need
    to work directly with the DBus protocol itself.

--- src/sdi-refresh-monitor.c
+++ src/sdi-refresh-monitor.c
@@ -18,8 +18,10 @@
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 #include <snapd-glib/snapd-glib.h>
+#include <unistd.h>
 #include <unity.h>
 
+#include "com.canonical.Unity.LauncherEntry.h"
 #include "sdi-helpers.h"
 #include "sdi-notify.h"
 #include "sdi-refresh-monitor.h"
@@ -46,6 +48,8 @@ struct _SdiRefreshMonitor {
   GtkWindow *main_window;
   GApplication *application;
   GtkBox *refresh_bar_container;
+  UnityComCanonicalUnityLauncherEntry *unity_manager;
+  GHashTable *refreshing_snap_list;
 };
 
 G_DEFINE_TYPE(SdiRefreshMonitor, sdi_refresh_monitor, G_TYPE_OBJECT)
@@ -68,12 +72,23 @@ G_DEFINE_AUTOPTR_CLEANUP_FUNC(SnapRefreshData, free_change_refresh_data);
 typedef struct {
   guint total_tasks;
   guint done_tasks;
+  gdouble old_progress;
+  gboolean done;
+  GPtrArray *desktop_files;
 } SnapProgressTaskData;
 
-static void free_progress_task_data(void *data) { g_free(data); }
+static void free_progress_task_data(void *data) {
+  SnapProgressTaskData *p = data;
+  g_ptr_array_unref(p->desktop_files);
+  g_free(data);
+}
 
-static SnapProgressTaskData *new_progress_task_data() {
-  return g_malloc0(sizeof(SnapProgressTaskData));
+static SnapProgressTaskData *new_progress_task_data(const gchar *snap_name) {
+  SnapProgressTaskData *retval = g_malloc0(sizeof(SnapProgressTaskData));
+  retval->old_progress = -1;
+  retval->done = FALSE;
+  retval->desktop_files = sdi_get_desktop_filenames_for_snap(snap_name);
+  return retval;
 }
 
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(SnapProgressTaskData, free_progress_task_data);
@@ -316,29 +331,38 @@ static void update_inhibited_snaps(SdiRefreshMonitor *self, SnapdChange *change,
 
 static void update_dock_bar(gpointer key, gpointer value, gpointer data) {
   SnapProgressTaskData *task_data = value;
-  const gchar *snap_name = key;
-  gboolean done = (GPOINTER_TO_INT(data) == 1);
+  g_autoptr(SdiRefreshMonitor) self = data;
 
-  g_autoptr(GPtrArray) desktop_files =
-      sdi_get_desktop_filenames_for_snap(snap_name);
   gdouble progress =
       ((gdouble)task_data->done_tasks) / ((gdouble)task_data->total_tasks);
-  for (int i = 0; i < desktop_files->len; i++) {
-    const gchar *desktop_file = desktop_files->pdata[i];
-    g_autoptr(UnityLauncherEntry) launcher =
-        unity_launcher_entry_get_for_desktop_id(desktop_file);
-    unity_launcher_entry_set_progress(launcher, progress);
-    unity_launcher_entry_set_progress_visible(launcher, !done);
+  if ((progress == task_data->old_progress) && !task_data->done) {
+    return;
+  }
+  task_data->done_tasks = 0;
+  task_data->total_tasks = 0;
+  task_data->old_progress = progress;
+  g_autoptr(GVariantBuilder) builder =
+      g_variant_builder_new(G_VARIANT_TYPE("a{sv}"));
+  g_variant_builder_add(builder, "{sv}", "progress",
+                        g_variant_new_double(progress));
+  g_variant_builder_add(builder, "{sv}", "progress-visible",
+                        g_variant_new_boolean(!task_data->done));
+  // values has a floating reference, so it's consumed when calling
+  // EMIT_UPDATE()
+  GVariant *values = g_variant_builder_end(builder);
+
+  for (int i = 0; i < task_data->desktop_files->len; i++) {
+    const gchar *desktop_file = task_data->desktop_files->pdata[i];
+    unity_com_canonical_unity_launcher_entry_emit_update(self->unity_manager,
+                                                         desktop_file, values);
   }
 }
 
 static void update_dock_snaps(SdiRefreshMonitor *self, SnapdChange *change,
                               gboolean done, gboolean hold) {
   GPtrArray *tasks = snapd_change_get_tasks(change);
-  // the key in this table is the snap name; the value is a SnapProgressTaskData
-  // structure
-  g_autoptr(GHashTable) snap_list = g_hash_table_new_full(
-      g_str_hash, g_str_equal, g_free, free_progress_task_data);
+  GSList *snaps_to_remove = NULL;
+
   for (gint i = 0; i < tasks->len; i++) {
     SnapdTask *task = tasks->pdata[i];
     SnapdTaskData *task_data = snapd_task_get_data(task);
@@ -353,20 +377,30 @@ static void update_dock_snaps(SdiRefreshMonitor *self, SnapdChange *change,
     for (gchar **p = affected_snaps; *p != NULL; p++) {
       gchar *snap_name = *p;
       SnapProgressTaskData *progress_task_data = NULL;
-      if (!g_hash_table_contains(snap_list, snap_name)) {
-        progress_task_data = new_progress_task_data();
-        g_hash_table_insert(snap_list, g_strdup(snap_name), progress_task_data);
+      if (!g_hash_table_contains(self->refreshing_snap_list, snap_name)) {
+        progress_task_data = new_progress_task_data(snap_name);
+        g_hash_table_insert(self->refreshing_snap_list, g_strdup(snap_name),
+                            progress_task_data);
       } else {
-        progress_task_data = g_hash_table_lookup(snap_list, snap_name);
+        progress_task_data =
+            g_hash_table_lookup(self->refreshing_snap_list, snap_name);
       }
       progress_task_data->total_tasks++;
+      progress_task_data->done = task_done;
       if (task_done) {
         progress_task_data->done_tasks++;
       }
+      if (done || hold) {
+        snaps_to_remove = g_slist_prepend(snaps_to_remove, g_strdup(snap_name));
+      }
     }
   }
-  g_hash_table_foreach(snap_list, update_dock_bar,
-                       GINT_TO_POINTER(done ? 1 : 0));
+  g_hash_table_foreach(self->refreshing_snap_list, update_dock_bar,
+                       g_object_ref(self));
+  for (GSList *p = snaps_to_remove; p != NULL; p = p->next) {
+    g_hash_table_remove(self->refreshing_snap_list, p->data);
+  }
+  g_slist_free_full(snaps_to_remove, g_free);
 }
 
 static void manage_change_update(SnapdClient *source, GAsyncResult *res,
@@ -500,6 +534,7 @@ static void sdi_refresh_monitor_dispose(GObject *object) {
   g_clear_object(&self->snapd_monitor);
   g_clear_object(&self->notify);
   g_clear_object(&self->application);
+  g_clear_pointer(&self->refreshing_snap_list, g_hash_table_unref);
   if (self->main_window != NULL) {
     gtk_window_destroy(self->main_window);
     self->main_window = NULL;
@@ -544,6 +579,10 @@ static void error_cb(GObject *object, GError *error, gpointer data) {
 void sdi_refresh_monitor_init(SdiRefreshMonitor *self) {
   self->snaps =
       g_hash_table_new_full(g_str_hash, g_str_equal, g_free, g_object_unref);
+  // the key in this table is the snap name; the value is a SnapProgressTaskData
+  // structure
+  self->refreshing_snap_list = g_hash_table_new_full(
+      g_str_hash, g_str_equal, g_free, free_progress_task_data);
   self->client = snapd_client_new();
   if (check_is_running_in_snap())
     snapd_client_set_socket_path(self->client, "/run/snapd-snap.socket");
@@ -611,6 +650,12 @@ SdiRefreshMonitor *sdi_refresh_monitor_new(GApplication *application) {
   SdiRefreshMonitor *self =
       g_object_new(SDI_TYPE_REFRESH_MONITOR, "notify", notify, NULL);
   self->application = g_object_ref(application);
+  g_autofree gchar *unity_object =
+      g_strdup_printf("/com/canonical/unity/launcherentry/%d", getpid());
+  self->unity_manager = unity_com_canonical_unity_launcher_entry_skeleton_new();
+  g_dbus_interface_skeleton_export(
+      G_DBUS_INTERFACE_SKELETON(self->unity_manager),
+      g_application_get_dbus_connection(application), unity_object, NULL);
   return self;
 }
 
