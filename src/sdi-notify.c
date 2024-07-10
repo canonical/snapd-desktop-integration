ae4c21b06f562e9f6ea0b1b7e52dcc1cc3707243
Sergio Costas <sergio.costas@canonical.com>
2024-07-11 00:26:09 +0200

    Udeng-2978: put the right icon in the top left corner (#77)
    * Put the right icon in the top-left corner
    
    Due to the fact that snap doesn't set a .desktop file for the
    snaps with the original name, but builds one with
    snap-name_app-name.desktop, the desktop identification usually
    fails. This makes GNotify to not be able to identify the .desktop
    file and set the right icon in the top-left corner of the
    notifications it sends.
    
    This patch migrates the notification code to libnotify, which
    allows to bypass this problem. But the original code is kept
    inside an #ifdef to allow to revert to it in the future, when
    this problem is fixed in snapd.

--- src/sdi-notify.c
+++ src/sdi-notify.c
@@ -21,6 +21,7 @@
 
 #include <gio/gdesktopappinfo.h>
 #include <glib/gi18n.h>
+#include <libnotify/notify.h>
 
 #include "sdi-helpers.h"
 #include "sdi-notify.h"
@@ -35,11 +36,129 @@ struct _SdiNotify {
 
 G_DEFINE_TYPE(SdiNotify, sdi_notify, G_TYPE_OBJECT)
 
+static void sdi_notify_action_ignore(GActionGroup *action_group,
+                                     GVariant *app_list, SdiNotify *self) {
+  gsize len;
+  g_autofree gchar **apps = (gchar **)g_variant_get_strv(app_list, &len);
+
+  g_return_if_fail(apps != NULL);
+  for (gsize i = 0; i < len; i++)
+    g_signal_emit_by_name(self, "ignore-snap-event", apps[i]);
+}
+
 static GTimeSpan get_remaining_time(SnapdSnap *snap) {
   g_autoptr(GDateTime) proceed_time = snapd_snap_get_proceed_time(snap);
   g_autoptr(GDateTime) now = g_date_time_new_now_local();
   return g_date_time_difference(proceed_time, now);
 }
+
+static GVariant *get_snap_list(GSList *snaps) {
+  if (snaps == NULL)
+    return NULL;
+  g_autoptr(GVariantBuilder) builder =
+      g_variant_builder_new(G_VARIANT_TYPE("as"));
+  for (; snaps != NULL; snaps = snaps->next) {
+    SnapdSnap *snap = (SnapdSnap *)snaps->data;
+    g_variant_builder_add(builder, "s", snapd_snap_get_name(snap));
+  }
+  return g_variant_ref_sink(g_variant_builder_end(builder));
+}
+
+// Currently, due to the way Snapd creates the .desktop files, the notifications
+// created with GNotify don't show the application icon in the upper-left
+// corner, putting instead the "generic gears" icon. This is the reason why, by
+// default, we use libnotify. This problem is being tackled by the snapd people,
+// so, in the future, GNotify would be the prefered choice.
+#ifndef USE_GNOTIFY
+
+static gchar *get_icon_name_from_gicon(GIcon *icon) {
+  if (icon == NULL) {
+    return NULL;
+  }
+
+  if (G_IS_THEMED_ICON(icon)) {
+    GThemedIcon *themed_icon = G_THEMED_ICON(icon);
+    const gchar *const *names = g_themed_icon_get_names(themed_icon);
+    if (names == NULL)
+      return NULL;
+    return g_strdup(names[0]);
+  }
+
+  if (G_IS_FILE_ICON(icon)) {
+    GFileIcon *file_icon = G_FILE_ICON(icon);
+    // the value returned by g_file_icon_get_file() is owned by the instance
+    return g_file_get_path(g_file_icon_get_file(file_icon));
+  }
+  return NULL;
+}
+
+typedef struct {
+  SdiNotify *self;
+  GVariant *snaps;
+} IgnoreNotifyData;
+
+G_DEFINE_AUTOPTR_CLEANUP_FUNC(NotifyNotification, g_object_unref)
+
+static void app_close_notification(NotifyNotification *notification,
+                                   char *action, gpointer user_data) {
+  g_object_unref(notification);
+}
+
+static void app_ignore_snaps_notification(NotifyNotification *notification,
+                                          char *action, gpointer user_data) {
+  IgnoreNotifyData *data = user_data;
+  sdi_notify_action_ignore(NULL, data->snaps, data->self);
+  g_variant_unref(data->snaps);
+  g_free(user_data);
+
+  g_object_unref(notification);
+}
+
+static void show_pending_update_notification(SdiNotify *self,
+                                             const gchar *title,
+                                             const gchar *body, GIcon *icon,
+                                             GSList *snaps) {
+  g_autofree gchar *icon_name = get_icon_name_from_gicon(icon);
+  // Don't use g_autoptr because it must survive for the actions
+  NotifyNotification *notification =
+      notify_notification_new(title, body, icon_name);
+  if (icon_name != NULL) {
+    // don't use g_autoptr with the GVariant because it is consumed in set_hint
+    notify_notification_set_hint(notification, "image-path",
+                                 g_variant_new_string(icon_name));
+  }
+  notify_notification_add_action(notification, "app.close-notification",
+                                 _("Close"), app_close_notification, NULL,
+                                 NULL);
+  notify_notification_add_action(notification, "default", _("Close"),
+                                 app_close_notification, NULL, NULL);
+  IgnoreNotifyData *data = g_malloc(sizeof(IgnoreNotifyData));
+  data->self = self;
+  data->snaps = get_snap_list(snaps);
+  notify_notification_add_action(notification, "app.ignore-notification",
+                                 _("Ignore"), app_ignore_snaps_notification,
+                                 data, NULL);
+  notify_notification_show(notification, NULL);
+}
+
+static void show_simple_notification(SdiNotify *self, const gchar *title,
+                                     const gchar *body, GIcon *icon,
+                                     const gchar *id) {
+  g_autofree gchar *icon_name = get_icon_name_from_gicon(icon);
+  // Don't use g_autoptr because it must survive for the actions
+  NotifyNotification *notification =
+      notify_notification_new(title, body, icon_name);
+
+  if (icon_name != NULL) {
+    notify_notification_set_hint(notification, "image-path",
+                                 g_variant_new_string(icon_name));
+  }
+  notify_notification_add_action(notification, "default", _("Close"),
+                                 app_close_notification, NULL, NULL);
+  notify_notification_show(notification, NULL);
+}
+
+#else
 
 static void show_pending_update_notification(SdiNotify *self,
                                              const gchar *title,
@@ -53,17 +172,12 @@ static void show_pending_update_notification(SdiNotify *self,
   }
   g_notification_set_default_action_and_target(
       notification, "app.close-notification", "s", "pending-update");
-  g_notification_add_button_with_target(
-      notification, "Close", "app.close-notification", "s", "pending-update");
-  g_autoptr(GVariantBuilder) builder =
-      g_variant_builder_new(G_VARIANT_TYPE("as"));
-  for (; snaps != NULL; snaps = snaps->next) {
-    SnapdSnap *snap = (SnapdSnap *)snaps->data;
-    g_variant_builder_add(builder, "s", snapd_snap_get_name(snap));
-  }
-  GVariant *values = g_variant_builder_end(builder);
-  g_notification_add_button_with_target_value(notification, "Ignore",
-                                              "app.ignore-updates", values);
+  g_notification_add_button_with_target(notification, _("Close"),
+                                        "app.close-notification", "s",
+                                        "pending-update");
+  g_autoptr(GVariant) snap_list = get_snap_list(snaps);
+  g_notification_add_button_with_target_value(notification, _("Ignore"),
+                                              "app.ignore-updates", snap_list);
   g_application_send_notification(self->application, "pending-update",
                                   notification);
 }
@@ -78,6 +192,7 @@ static void show_simple_notification(SdiNotify *self, const gchar *title,
   }
   g_application_send_notification(self->application, id, notification);
 }
+#endif
 
 void sdi_notify_pending_refresh_one(SdiNotify *self, SnapdSnap *snap) {
   g_return_if_fail(SDI_IS_NOTIFY(self));
@@ -178,16 +293,6 @@ void sdi_notify_refresh_complete(SdiNotify *self, SnapdSnap *snap,
 GApplication *sdi_notify_get_application(SdiNotify *self) {
   g_return_val_if_fail(SDI_IS_NOTIFY(self), NULL);
   return self->application;
-}
-
-static void sdi_notify_action_ignore(GActionGroup *action_group,
-                                     GVariant *app_list, SdiNotify *self) {
-  gsize len;
-  g_autofree gchar **apps = (gchar **)g_variant_get_strv(app_list, &len);
-
-  g_return_if_fail(apps != NULL);
-  for (gsize i = 0; i < len; i++)
-    g_signal_emit_by_name(self, "ignore-snap-event", apps[i]);
 }
 
 static void set_actions(SdiNotify *self) {
