afbac3896cb9bd3cd0ea889e0a477a478cdb027e
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2025-01-17 13:20:48 +0100

    Update to last format-source.sh script

--- format-source.sh
+++ format-source.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# The SOURCE_PATHS environment variable is an optional ENV where a
+# space-separated list of paths can be configured, where the script
+# will search for header and source files. If not defined, the script
+# will search recursively in all the project folders
 
 if [ $# -eq 0 ]; then
     FORMAT_CODE=1
@@ -34,6 +39,17 @@ check_source_file() {
     fi
 }
 
+RESULT=""
+
+if [ -n "${SOURCE_PATHS}" ]; then
+# if SOURCE_PATHS is defined, search only in the specified paths
+    for source_path in ${SOURCE_PATHS}; do
+        RESULT=${RESULT}$(find $source_path -name '*.c' -or -name '*.h' | while read file; do
+            check_source_file "$file"
+        done)
+    done
+else
+
 # a trick to ensure that names/folders with blank spaces do work as expected
 # also, since find and while read... are executed in a subshell, they can't
 # modify any variable outside. That's why we capture the output in $RESULT
@@ -42,9 +58,10 @@ check_source_file() {
 # STDERR. That's why the check_source_file() function has so many >&2: those
 # are commands whose output we want in the screen. This trick is needed because
 # there seems to not be an easy way of storing STDERR instead of SDTOUT
-RESULT=$(find -name '*.c' -or -name '*.h' | while read file; do
-    check_source_file "$file"
-done)
+    RESULT=${RESULT}$(find -name '*.c' -or -name '*.h' | while read file; do
+        check_source_file "$file"
+    done)
+fi
 
 if [ ! "$RESULT" = "" ]; then
     echo Failed to pass clang-format check
